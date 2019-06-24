48013f323df8715efe63da0dfb8bf7a26d9b2f36
mariomorvan <mario.morvan.18@ucl.ac.uk>
2019-06-24 15:28:59 +0200

    basic linear transit testing

--- tests/test_transit.py
+++ tests/test_transit.py
@@ -1,2 +1,13 @@
+import numpy as np
+from utils.transit import transit_linear, fit_transit_linear
 
-raise NotImplementedError
+N = 100
+time_array = np.linspace(0, 1, N)
+pars = 0.55, 0.1, 0.6, 0.1
+
+
+def test_transit_linear():
+    x = transit_linear(time_array, *pars)
+    x = np.expand_dims(x, 0)
+    pred = fit_transit_linear(x, time_array)
+    np.testing.assert_allclose(pred, pars)
