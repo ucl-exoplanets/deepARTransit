f731ab8740f2ee33bb9612911c3bec0db8c23e54
mariomorvan <mario.morvan.18@ucl.ac.uk>
2019-06-27 17:32:33 +0200

    minor fix

--- utils/dirs.py
+++ utils/dirs.py
@@ -9,7 +9,7 @@ def create_dirs(dirs):
     """
     try:
         for dir_ in dirs:
-            if not os.path.exists(dir_):
+            if not os.path.isdir(dir_):
                 os.makedirs(dir_)
         return 0
     except Exception as err:
