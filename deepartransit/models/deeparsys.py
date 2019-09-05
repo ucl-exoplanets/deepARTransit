ed8588a0e8d4100dfaddf23db23f9ffcc6636103
mariomorvan <mario.morvan.18@ucl.ac.uk>
2019-09-05 19:34:04 +0100

    early stopping with mse_pred

--- deepartransit/models/deeparsys.py
+++ deepartransit/models/deeparsys.py
@@ -221,6 +221,9 @@ class DeepARSysTrainer(BaseTrainer):
         ###############
         pred_range = range(self.config.pretrans_length, self.config.pretrans_length+self.config.trans_length)
         mse_pred = np.mean(((np.take(self.data.Z, pred_range, axis=1) - np.take(locs, pred_range, axis=0).swapaxes(0,1)))**2)
+
+        if self.config.early_stop:
+            self.early_stop_metric_list.append(mse_pred)
         # TENSORBOARD eval summary
         lr = self.model.learning_rate_tensor.eval()
         summaries_dict = {
